0ec15bdbd42f690879aac0bb9d07e316b3bf3012
Sumit00s <sonawanesumit245@gmail.com>
2025-09-22 09:15:22 +0530

    Multi Model Layout Created

---
+++ app/_components/AppHeader.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+
+export default function AppHeader() {
+  return (
+    <div className="p-3 w-full shadow flex justify-between items-center">
+      <SidebarTrigger />
+      <Button>Sign In</Button>
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import ChatInputBox from "./_components/ChatInputBox";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <ChatInputBox />
+    </div>
+  );
 }
