6ed0672c6c0429b44acf8f668c84bf8db9a286b8
Sumit00s <sonawanesumit245@gmail.com>
2025-09-22 12:27:11 +0530

    Save user model Prefrence Feature added

--- app/provider.jsx
+++ app/provider.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import AppSidebar from "./_components/AppSidebar";
@@ -8,9 +8,15 @@ import AppHeader from "./_components/AppHeader";
 import { useUser } from "@clerk/nextjs";
 import { db } from "@/config/FirebaseConfig";
 import { doc, getDoc, setDoc } from "firebase/firestore";
+import { AiSelectedModelContext } from "@/context/AiSelectedModelContext";
+import { DefaultModel } from "@/shared/AiModelsShared";
+import { UserDetailContext } from "@/context/UserDetailContext";
 
 export default function Provider({ children, ...props }) {
   const { user } = useUser();
+
+  const [aiSelectedModels, setAiSelectedModels] = useState(DefaultModel);
+  const [userDetail, setUserDetail] = useState();
 
   useEffect(() => {
     if (user) CreateNewUser();
@@ -28,6 +34,9 @@ export default function Provider({ children, ...props }) {
 
     if (userSnap.exists()) {
       console.log("Existing User");
+      const userInfo = userSnap.data();
+      setAiSelectedModels(userInfo?.selectedModelPref);
+      setUserDetail(userInfo);
       return;
     } else {
       const userData = {
@@ -40,6 +49,7 @@ export default function Provider({ children, ...props }) {
       };
 
       await setDoc(userRefrence, userData);
+      setUserDetail(userData);
       console.log("New User Data Saved");
     }
   };
@@ -52,14 +62,20 @@ export default function Provider({ children, ...props }) {
       enableSystem
       disableTransitionOnChange
     >
-      <SidebarProvider>
-        <AppSidebar />
+      <UserDetailContext.Provider value={{ userDetail, setUserDetail }}>
+        <AiSelectedModelContext.Provider
+          value={{ aiSelectedModels, setAiSelectedModels }}
+        >
+          <SidebarProvider>
+            <AppSidebar />
 
-        <div className="w-full">
-          <AppHeader />
-          {children}
-        </div>
-      </SidebarProvider>
+            <div className="w-full">
+              <AppHeader />
+              {children}
+            </div>
+          </SidebarProvider>
+        </AiSelectedModelContext.Provider>
+      </UserDetailContext.Provider>
     </NextThemesProvider>
   );
 }
